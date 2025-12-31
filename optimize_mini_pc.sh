f4aefb2be23d7c30aa9baca8a281891237b016df
snaplyze <github@snaplyze.me>
2026-01-01 01:36:06 +0300

    feat(minipc): sync zsh config with vps script
    Added missing safety aliases (rm -i, etc.) and utility functions (mkcd, extract) to optimize_mini_pc.sh for parity with optimize_vps.sh.

--- optimize_mini_pc.sh
+++ optimize_mini_pc.sh
@@ -808,6 +808,38 @@ alias remove='sudo apt remove'
 alias search='apt search'
 alias autoremove='sudo apt autoremove -y'
 
+# Safety aliases
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias mkdir='mkdir -pv'
+
+# Useful functions
+mkcd() {
+    mkdir -p "$1" && cd "$1"
+}
+
+extract() {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xjf $1     ;;
+            *.tar.gz)    tar xzf $1     ;;
+            *.bz2)       bunzip2 $1     ;;
+            *.rar)       unrar e $1     ;;
+            *.gz)        gunzip $1      ;;
+            *.tar)       tar xf $1      ;;
+            *.tbz2)      tar xjf $1     ;;
+            *.tgz)       tar xzf $1     ;;
+            *.zip)       unzip $1       ;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1        ;;
+            *)           echo "'$1' cannot be extracted via extract()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
+
 # Go/NVM paths
 export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 
